82de78b49b1ddace59a51fbd3e6c1c64a8b4e5f6
ranrandy <runfeng4@illinois.edu>
2021-04-07 01:58:50 +0800

    Generate a trained model with k = 100

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -4,16 +4,15 @@
 
 using namespace naivebayes;
 
-// TODO: You may want to change main's signature to take in argc and argv
 int main() {
-  // TODO: Replace this with code that reads the training data, trains a model,
-  // and saves the trained model to a file.
   std::ifstream input_file("/Users/lirunfeng/cinder_master/my-projects/"
                            "naive-bayes-ranrandy/data/trainingimagesandlabels"
                            ".txt");
-  DataConverter data_converter;
+  
   if (input_file.is_open()) {
+    DataConverter data_converter;
     input_file >> data_converter;
+    
     DataProcessor data_processor(data_converter, 100);
     std::ofstream output_file("/Users/lirunfeng/cinder_master/my-projects/"
                               "naive-bayes-ranrandy/data/computed_probabilities"
