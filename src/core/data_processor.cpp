4d0ad449de9b558dad0c9dad64a499118d4eb77a
ranrandy <runfeng4@illinois.edu>
2021-04-06 16:23:54 +0800

    Improve calculating probabilities for pixels

--- src/core/data_processor.cpp
+++ src/core/data_processor.cpp
@@ -13,7 +13,8 @@ DataProcessor::DataProcessor(
           data_converter.GetImageSize(),
           vector<vector<double>>(
               data_converter.pixel_color_count_,
-              vector<double>(data_converter.greatest_written_number + 1,0))));
+              vector<double>(
+                  data_converter.greatest_written_number + 1, 0))));
   CalculateProbabilityForPixels(data_converter);
 }
 
@@ -29,15 +30,14 @@ vector<vector<vector<vector<double>>>> DataProcessor::GetPixelProbability()
 void DataProcessor::CalculateProbabilityForClasses(
     const DataConverter& data_converter) {
   std::vector<WrittenNumber> written_numbers = data_converter.GetDataset();
-  std::map<size_t, size_t> image_class_count;
 
   for (const WrittenNumber& written_number : written_numbers) {
     if (written_number.GetImageClass() >= 0) {
-      image_class_count[written_number.GetImageClass()]++;
+      class_probabilities_[written_number.GetImageClass()]++;
     }
   }
 
-  for (auto & it : image_class_count) {
+  for (auto & it : class_probabilities_) {
      class_probabilities_[it.first] =
         (laplace_parameter + it.second) /
         (data_converter.image_class_count_ * laplace_parameter +
@@ -55,13 +55,28 @@ void DataProcessor::CalculateProbabilityForPixels(
         pixel_probabilities_[i][j]
                             [static_cast<size_t>(
                                 written_number.GetImageVector()[i][j])]
-                            [written_number.GetImageClass()] +=
-            1.0 / (class_probabilities_[written_number.GetImageClass()] *
-                   written_numbers.size());
+                            [written_number.GetImageClass()] =
+            laplace_parameter /
+            (class_probabilities_[written_number.GetImageClass()] *
+                 written_numbers.size() +
+             data_converter.pixel_color_count_ * laplace_parameter);
       }
     }
   }
 
+  for (const WrittenNumber& written_number : written_numbers) {
+    for (size_t i = 0; i < written_number.GetImageVector().size(); i++) {
+      for (size_t j = 0; j < written_number.GetImageVector()[i].size(); j++) {
+        pixel_probabilities_[i][j]
+                            [static_cast<size_t>(
+                                written_number.GetImageVector()[i][j])]
+                            [written_number.GetImageClass()] +=
+            1.0 / (class_probabilities_[written_number.GetImageClass()] *
+                   written_numbers.size() +
+                   data_converter.pixel_color_count_ * laplace_parameter);
+      }
+    }
+  }
 }
 
 } // namespace naivebayes
