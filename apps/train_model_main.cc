683aec32b95eedf9e6ee3259a53447c8faab6aab
ranrandy <runfeng4@illinois.edu>
2021-04-07 11:53:45 +0800

    Implement command line input parameters

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -4,20 +4,35 @@
 
 using namespace naivebayes;
 
-int main() {
-  std::ifstream input_file("/Users/lirunfeng/cinder_master/my-projects/"
-                           "naive-bayes-ranrandy/data/trainingimagesandlabels"
-                           ".txt");
-  if (input_file.is_open()) {
-    DataConverter data_converter;
-    input_file >> data_converter;
+int main(int argc, char* argv[]) {
+  // argv[1] = load_model_path
+  if (argc == 2) {
+    std::ifstream input_file(argv[1]);
     
-    DataProcessor data_processor(data_converter, 100);
-    std::ofstream output_file("/Users/lirunfeng/cinder_master/my-projects/"
-                              "naive-bayes-ranrandy/data/computed_probabilities"
-                              ".txt");
-    output_file << data_processor;
-    input_file.close();
+    if (input_file.is_open()) {
+      DataProcessor data_processor;
+      input_file >> data_processor;
+      input_file.close();
+    }
+  }
+
+  // argv[1] = laplace parameter
+  // argv[2] = dataset_path
+  // argv[3] = save_model_path
+  if (argc == 4) {
+    std::ifstream input_file(argv[2]);
+
+    if (input_file.is_open()) {
+      DataConverter data_converter;
+      input_file >> data_converter;
+
+      DataProcessor data_processor(data_converter, std::stod(argv[1]));
+      std::ofstream output_file(argv[3]);
+      output_file << data_processor;
+
+      output_file.close();
+      input_file.close();
+    }
   }
   return 0;
 }
