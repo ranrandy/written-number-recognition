6093e8f54a4be43c64c530002d79ac52697115b6
ranrandy <runfeng4@illinois.edu>
2021-04-11 02:50:45 +0800

    Implement command line parser

---
+++ include/core/cmd_parser.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "tclap/CmdLine.h"
+
+namespace naivebayes {
+
+using TCLAP::CmdLine;
+using std::string;
+
+/**
+ * Parse the command line input to run the project.
+ */
+class CmdParser {
+public:
+  /**
+   * Constructor for CmdParser. Create the parsing methods and parse the 
+   * command line input.
+   * @param argc the number of parameters from command line
+   * @param argv the parameters from command line
+   */
+  CmdParser(int argc, char* argv[]);
+  
+  /**
+   * Gets the dataset file path.
+   * @return dataset file path
+   */
+  const string& GetDatasetFilePath() const;
+  
+  /**
+   * Gets the test file path.
+   * @return test file path
+   */
+  const string& GetTestFilePath() const;
+  
+  /**
+   * Gets the model file path
+   * @return model file path
+   */
+  const string& GetModelFilePath() const;
+  
+  /**
+   * Gets the path where model will be saved to.
+   * @return the path where model will be saved to
+   */
+  const string& GetSaveFilePath() const;
+  
+  /**
+   * Gets the algorithm that will be used to train the dataset.
+   * @return the algorithm that will be used to train the dataset
+   */
+  const string& GetAlgorithm() const;
+  
+  /**
+   * Gets the laplace smoothing parameter k for naive bayes algorithm.
+   * @return the laplace smoothing parameter k for naive bayes algorithm
+   */
+  double GetNaiveBayesK() const;
+
+private:
+  const string kNaiveBayes = "naive_bayes";
+  const string kKNearestNeighbor = "knn";
+  const string kArtificialNeuralNetwork = "ann";
+  const string kDecisionTree = "decision_tree";
+  const string kGaussianNaiveBayes = "g_naive_bayes";
+  const string kVotingBoosting = "voting_boosting";
+  const string kNoFile = "no_file";
+  const string kProjectVersion = "0.1";
+  const string kStringReturnType = "string";
+  const char kDelimiter = ' ';
+
+  string dataset_file_path_;
+  string test_file_path_;
+  string model_file_path_;
+  string save_file_path_;
+  string algorithm_;
+  
+  double naive_bayes_k;
+};
+
+} // namespace naivebayes
+
---
+++ src/core/cmd_parser.cpp
@@ -0,0 +1,91 @@
+#include "core/cmd_parser.h"
+
+namespace naivebayes {
+
+using TCLAP::CmdLine;
+using TCLAP::ValueArg;
+using TCLAP::ValuesConstraint;
+using std::vector;
+using std::string;
+
+CmdParser::CmdParser(int argc, char **argv) {
+  try {
+    CmdLine cmd("Command description message", kDelimiter, kProjectVersion);
+
+    // Specify the dataset file to read from 
+    // or the model to load from
+    ValueArg<string> read_arg("", "read", "read a dataset", false, kNoFile,
+                              kStringReturnType);
+    ValueArg<string> load_arg("", "load", "load a model", false, kNoFile,
+                              kStringReturnType);
+    cmd.xorAdd(read_arg, load_arg);
+    
+    // Specify the parameter for an algorithm
+    ValueArg<double> parameter_arg("", "naive_bayes_k", 
+                                   "laplace parameter for naive bayes "
+                                   "algorithm", false, 0, "double");
+    cmd.add(parameter_arg);
+
+    // Specify whether to save the trained model to a file or not
+    ValueArg<string> save_arg("s", "save", "save to file", false, kNoFile,
+                              kStringReturnType);
+    cmd.add(save_arg);
+
+    // Specify a test file to test
+    ValueArg<string> test_arg("", "test", "test a dataset", false, kNoFile,
+                              kStringReturnType);
+    cmd.add(test_arg);
+
+    // Specify the algorithm to train the dataset with
+    vector<string> models;
+    models.emplace_back(kNaiveBayes);
+    models.emplace_back(kKNearestNeighbor);
+    models.emplace_back(kGaussianNaiveBayes);
+    models.emplace_back(kDecisionTree);
+    models.emplace_back(kVotingBoosting);
+    models.emplace_back(kArtificialNeuralNetwork);
+    ValuesConstraint<string> model_names(models);
+    ValueArg<string> algorithm_arg("", "train", "algorithm to train with",
+                                   false, kNaiveBayes, &model_names);
+    cmd.add(algorithm_arg);
+    
+    // Parse each block of the entire command line input
+    cmd.parse(argc, argv);
+    
+    dataset_file_path_ = read_arg.getValue();
+    test_file_path_ = test_arg.getValue();
+    model_file_path_ = load_arg.getValue();
+    save_file_path_ = save_arg.getValue();
+    algorithm_ = algorithm_arg.getValue();
+    naive_bayes_k = parameter_arg.getValue();
+  } catch (TCLAP::ArgException &e) {
+    std::cerr << "error: " << e.error() << " for arg " << e.argId() << 
+        std::endl;
+  }
+}
+
+const string& CmdParser::GetDatasetFilePath() const {
+  return dataset_file_path_;
+}
+
+const string& CmdParser::GetTestFilePath() const {
+  return test_file_path_;
+}
+
+const string& CmdParser::GetModelFilePath() const {
+  return model_file_path_;
+}
+
+const string& CmdParser::GetSaveFilePath() const {
+  return save_file_path_;
+}
+
+const string& CmdParser::GetAlgorithm() const {
+  return algorithm_;
+}
+ 
+double CmdParser::GetNaiveBayesK() const {
+  return naive_bayes_k;
+}
+
+} // naivebayes
